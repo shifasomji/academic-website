eea474c0ea9e6aeb385f558d19443b723bb6d7fa
Shifa Somji <shifasomji@Shifas-MacBook-Air.local>
2024-08-13 19:16:52 -0400

    news items

--- src/content/news_items.js
+++ src/content/news_items.js
@@ -1,7 +1,7 @@
 import Graduation from '../media/Graduation.jpg';
 import newsitem_pic from '../media/newsitem_pic.jpg';
-import RLCIE from '../media/RLCIE.png';
 import Regenstrief from '../media/Regenstrief.png';
+import HMC from '../media/HMC.png';
 
 export const news_items = [
   {
@@ -12,15 +12,10 @@ export const news_items = [
   {
     pic: Graduation,
     year: 2023,
-    text_p: <p>Graduated from HMC with high distinction and honors. Earned the <a href="https://www.hmc.edu/cs/student-awards/">Class of '94</a> award, the CS department's top award.</p>
+    text_p: <p>Graduated from HMC with high distinction and honors. Earned the <a href="https://www.hmc.edu/cs/student-awards/">Class of '94</a> award, the CS department's top award, and the RLCIE Fellow designation.</p>
   },
   {
-    pic: RLCIE,
-    year: 2023,
-    text_p: <p>Received the <a href="https://www.cmc.edu/rlcie/community/rlcie-alumni">RLCIE Fellow</a> designation</p>
-  },
-  {
-    pic: newsitem_pic,
+    pic: HMC,
     year: 2021,
     text_p: <p>Received the <a href="https://www.hmc.edu/about/2024/05/01/celebrating-hmc-community-leaders/#:~:text=Dean%20Sundberg%20Prize,-Ethan%20Sandoval%20'26&text=Established%20by%20Dana%20Seaton%20'06,impact%20on%20the%20College%20community.">Dean Chris Sundberg Prize</a>, recognizing a HMC junior who demonstrates exceptional leadership and has a positive impact on HMC community.</p>
   },
