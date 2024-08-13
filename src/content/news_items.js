d417bd1d2e8d22f2f33ba3122dea00b811baaa48
Shifa Somji <shifasomji@Shifas-MacBook-Air.local>
2024-08-13 19:03:29 -0400

    images for news items

--- src/content/news_items.js
+++ src/content/news_items.js
@@ -1,25 +1,28 @@
 import Graduation from '../media/Graduation.jpg';
 import newsitem_pic from '../media/newsitem_pic.jpg';
+import RLCIE from '../media/RLCIE.png';
+import Regenstrief from '../media/Regenstrief.png';
 
 export const news_items = [
+  {
+    pic: Regenstrief,
+    year: 2024,
+    text_p: <p>Received the <a href="https://www.purdue.edu/research/rche/students/student-support.php">John W. Anderson Foundation</a> scholarship, which supports researchers interested in healthcare engineering.</p>
+  },
   {
     pic: Graduation,
     year: 2023,
     text_p: <p>Graduated from HMC with high distinction and honors. Earned the <a href="https://www.hmc.edu/cs/student-awards/">Class of '94</a> award, the CS department's top award.</p>
   },
   {
-    pic: newsitem_pic,
-    year: 2024,
-    text_p: <p>Received the <a href="https://www.purdue.edu/research/rche/students/student-support.php">John W. Anderson Foundation</a> scholarship, which supports researchers interested in healthcare engineering.</p>
+    pic: RLCIE,
+    year: 2023,
+    text_p: <p>Received the <a href="https://www.cmc.edu/rlcie/community/rlcie-alumni">RLCIE Fellow</a> designation</p>
   },
   {
     pic: newsitem_pic,
     year: 2021,
     text_p: <p>Received the <a href="https://www.hmc.edu/about/2024/05/01/celebrating-hmc-community-leaders/#:~:text=Dean%20Sundberg%20Prize,-Ethan%20Sandoval%20'26&text=Established%20by%20Dana%20Seaton%20'06,impact%20on%20the%20College%20community.">Dean Chris Sundberg Prize</a>, recognizing a HMC junior who demonstrates exceptional leadership and has a positive impact on HMC community.</p>
   },
-  {
-    pic: newsitem_pic,
-    year: 2020,
-    text_p: <p>Non sodales neque sodales ut etiam sit amet nisl. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.</p>
-  },
+
 ];
