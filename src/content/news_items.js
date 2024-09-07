2c400f5fa8e96fd30d1cfea4d4773eb6a8f91acf
shifasomji <shifamsomji@gmail.com>
2024-09-07 15:33:56 -0400

    Dodds picture

--- src/content/news_items.js
+++ src/content/news_items.js
@@ -1,7 +1,8 @@
-import Graduation from '../media/Klawe.jpg';
+import Graduation from '../media/Klawe.png';
 import newsitem_pic from '../media/newsitem_pic.jpg';
 import Regenstrief from '../media/Regenstrief.png';
 import HMC from '../media/HMC.png';
+import Dodds from '../media/Graduation.jpg';
 
 export const news_items = [
   {
@@ -15,7 +16,7 @@ export const news_items = [
     text_p: <p>Graduated from HMC with high distinction and honors. Earned the <a href="https://www.hmc.edu/cs/student-awards/">Class of '94</a> award, the CS department's top award, and the RLCIE Fellow designation.</p>
   },
   {
-    pic: HMC,
+    pic: Dodds,
     year: 2021,
     text_p: <p>Received the <a href="https://www.hmc.edu/about/2024/05/01/celebrating-hmc-community-leaders/#:~:text=Dean%20Sundberg%20Prize,-Ethan%20Sandoval%20'26&text=Established%20by%20Dana%20Seaton%20'06,impact%20on%20the%20College%20community.">Dean Chris Sundberg Prize</a>, recognizing a HMC junior who demonstrates exceptional leadership and has a positive impact on HMC community.</p>
   },
